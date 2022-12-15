6c3e474bfab9ef4a17b6e6894737e43eebe20045
adeak <adeak@users.noreply.github.com>
2022-12-15 23:28:19 +0100

    Day 15, use tuples instead of ranges

--- day15.py
+++ day15.py
@@ -9,9 +9,9 @@ def manhattan(pos0, pos1):
 
 
 def compute_ranges(distances, y0):
-    """Compute a set of merged ranges covered by any sensor in a given row at y.
+    """Compute a set of merged "ranges" covered by any sensor in a given row at y.
 
-    Returns a list of ranges (these might still contain beacons).
+    Returns a list of inclusive (from, to) tuples (these might still contain beacons).
     """
     # for the given y0, gather the intervals that are covered by each sensor
     covered_ranges = set()
@@ -22,21 +22,20 @@ def compute_ranges(distances, y0):
             continue
         xmin = x - (dist - abs(dy))
         xmax = x + (dist - abs(dy))
-        covered_ranges.add(range(xmin, xmax + 1))
+        covered_ranges.add((xmin, xmax))
 
     # pre-process ranges: merge overlaps
-    covered_ranges = sorted(covered_ranges, key=attrgetter('start'))
+    covered_ranges = sorted(covered_ranges)
     merged_ranges = []
     current_range = covered_ranges[0]
     for next_range in covered_ranges[1:]:
         # check if current and next overlap
         # or just touch!
-        overlap = current_range.start in next_range or next_range.start in current_range
-        touching = current_range.start == next_range.stop or next_range.start == current_range.stop
-        if overlap or touching:
+        overlap_or_touching = next_range[0] <= current_range[1] + 1
+        if overlap_or_touching:
             # merge these
-            stop = max(current_range.stop, next_range.stop)
-            current_range = range(current_range.start, stop)
+            end = max(current_range[1], next_range[1])
+            current_range = (current_range[0], end)
         else:
             # no overlap
             merged_ranges.append(current_range)
@@ -68,26 +67,30 @@ def day15(inp, testing=False):
     for y0 in y0s:
         # compute merged ranges from each beacon
         covered_ranges = compute_ranges(distances, y0)
+
         if y0 == special_y:
             # part 1: find length of merged ranges sans beacons
             #         (and hope that part 2's answer comes later)
-            covereds = sum(len(rng) for rng in covered_ranges)
+            covereds = sum(rng[1] - rng[0] + 1 for rng in covered_ranges)
             beaconeds = sum(
                 1
                 for beacon in beacons
-                if beacon[1] == y0 and any(beacon[0] in rng for rng in covered_ranges)
+                if beacon[1] == y0 and any(rng[0] <= beacon[0] <= rng[1] for rng in covered_ranges)
             )
             excludeds = covereds - beaconeds
+
         # part 2: find lonely beacon
+        first_range = covered_ranges[0]
+        # (premise: we have either one or two ranges)
         if len(covered_ranges) == 2:
             # there's a hole in the middle
-            lonely_beacon = (covered_ranges[0].stop, y0)
+            lonely_beacon = (first_range[1] + 1, y0)
             break
-        elif 0 not in covered_ranges[0]:
+        elif first_range[0] > 0:
             # uncovered left edge
             lonely_beacon = (0, y0)
             break
-        elif ymax not in covered_ranges[0]:
+        elif first_range[1] < ymax:
             # uncovered right edge; ymax is also xmax
             lonely_beacon = (ymax, y0)
             break
