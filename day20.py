898bc8721b4a558c7638684163dcc73513bfd57f
adeak <adeak@users.noreply.github.com>
2022-12-20 17:45:12 +0100

    Day 20

--- day20.py
+++ day20.py
@@ -1,44 +1,43 @@
 from collections import deque
 
 def get_coordinate_sum(order, nums):
+    # find index corresponding to 0, find 3 successors-by-1000-modulo-size
     origin = next(i for i, index in enumerate(order) if nums[index] == 0)
-    coords = []
-    order = deque(order)
-    order.rotate(-origin)
-    for _ in range(3):
-        order.rotate(-1000)
-        coords.append(nums[order[0]])
-    return sum(coords)
+    return sum(nums[order[i % len(nums)]] for i in range(origin + 1000, origin + 3001, 1000))
+
 
 def mix_nums(order, nums):
-    # brute force for part 1...
+    # deque does the heavy lifting
     for i, num in enumerate(nums):
-        index_now = order.index(i)
-        order.rotate(-index_now)
-        ind_sanitycheck = order.popleft()
-        assert ind_sanitycheck == i
-        order.rotate(-num)
-        order.appendleft(i)
+        index_now = order.index(i)  # find where current number's index is
+        order.rotate(-index_now)  # move it to the left
+        order.popleft()  # == i
+        order.rotate(-num)  # let deque do the math for large numbers
+        order.appendleft(i)  # insert at new position
     return order
 
 
-def day20(inp):
+def day20(inp, part2=False):
     lines = inp.rstrip().splitlines()
     nums = list(map(int, lines))
 
-    # operate on original indices for uniqueness
+    if part2:
+        n_iters = 10
+        nums = [num * 811589153 for num in nums]
+    else:
+        n_iters = 1
+
+    # operate on original indices instead of raw numbers for uniqueness
     order = deque(range(len(nums)), maxlen=len(nums))
-    for it in range(1):
+    for it in range(n_iters):
         order = mix_nums(order, nums)
 
-        if it == 0:
-            part1 = get_coordinate_sum(order, nums)
-
-    return part1#, part2
+    coordinate_sum = get_coordinate_sum(order, nums)
+    return coordinate_sum
 
 
 if __name__ == "__main__":
     testinp = open('day20.testinp').read()
-    print(day20(testinp))
+    print(day20(testinp), day20(testinp, part2=True))
     inp = open('day20.inp').read()
-    print(day20(inp))
+    print(day20(inp), day20(inp, part2=True))
